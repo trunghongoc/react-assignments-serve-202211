0e32c00846828ae6c063b2ff32a2c7c82ce5ff42
Trung Ho Ngoc - macpro 13 2018 <trung0194@gmai.com>
2022-11-07 20:13:56 +0700

    okela 2

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -1,10 +1,21 @@
 // import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { faker } from '@faker-js/faker'
 
 import User from 'App/Models/User'
 
 export default class UsersController {
   public async index() {
     const users = await User.all()
+
+    return users
+  }
+
+  public async addRandomUser() {
+    const users = await User.create({
+      email: faker.internet.email(),
+      name: faker.name.fullName(),
+      password: '123456',
+    })
 
     return users
   }
--- start/routes.ts
+++ start/routes.ts
@@ -22,6 +22,7 @@ import Route from '@ioc:Adonis/Core/Route'
 
 Route.group(() => {
   Route.get('/users', 'UsersController.index')
+  Route.get('/users/create-random', 'UsersController.addRandomUser')
 }).prefix('/api')
 
 Route.get('*', async ({ view }) => {
